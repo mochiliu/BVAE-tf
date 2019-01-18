81ddf619b4565a87e1b11a22096afb250c450160
Mochi Liu <youxi3013@126.com>
2019-01-18 18:11:07 -0500

    production ready

--- bvae/gol_ae.py
+++ bvae/gol_ae.py
@@ -60,37 +60,39 @@ if __name__ == "__main__":
     twrv_one.start()
     img=twrv_one.join()
     
-    while iteration_number < 100000:
+    while iteration_number < 1000000:
         twrv_one = ThreadWithReturnValue(target=manager.get_images, args=(batchSize,))
         twrv_one.start()
         
         while twrv_one.is_alive():
             bvae.ae.fit(img, img, epochs=1, batch_size=batchSize, verbose=0)
             iteration_number+=1
-
+            if iteration_number % 100 == 0:
+                #print(str(iteration_number)+time.ctime())
+            if iteration_number % 10000 == 0:
+                latentVec = bvae.encoder.predict(img, batch_size=batchSize)[0]
+                print(latentVec)
+                print(time.ctime())
+                
+                train = img[0] #get a sample image
+                train[train > 0.5] = 0.5 # clean it up a bit
+                train[train < -0.5] = -0.5
+                train = np.uint8((train + 0.5)* 255) # convert to regular image values
+                train = Image.fromarray(train)
+                train.save('./outputs/train_'+str(iteration_number)+'.bmp')
+        
+                pred = bvae.ae.predict(img, batch_size=batchSize)[0] # get the reconstructed image
+                pred[pred > 0.5] = 0.5 # clean it up a bit
+                pred[pred < -0.5] = -0.5
+                pred = np.uint8((pred + 0.5)* 255) # convert to regular image values
+                pred = Image.fromarray(pred)
+                pred.save('./outputs/pred_'+str(iteration_number)+'.bmp')
+                
+                bvae.ae.save('./output_models/'+str(iteration_number)+'_autoencoder.h5')
+                bvae.decoder.save('./output_models/'+str(iteration_number)+'_decoder.h5')
+                bvae.encoder.save('./output_models/'+str(iteration_number)+'_encoder.h5')
         img=twrv_one.join()
-        #print(str(iteration_number)+time.ctime())
+        
         #check in once n iterations
-        if iteration_number % 5000 = 0:
-            latentVec = bvae.encoder.predict(img, batch_size=batchSize)[0]
-            print(latentVec)
-            print(time.ctime())
-            
-            train = img[0] #get a sample image
-            train[train > 0.5] = 0.5 # clean it up a bit
-            train[train < -0.5] = -0.5
-            train = np.uint8((train + 0.5)* 255) # convert to regular image values
-            train = Image.fromarray(train)
-            train.save('./outputs/train_'+str(iteration_number)+'.bmp')
-    
-            pred = bvae.ae.predict(img, batch_size=batchSize)[0] # get the reconstructed image
-            pred[pred > 0.5] = 0.5 # clean it up a bit
-            pred[pred < -0.5] = -0.5
-            pred = np.uint8((pred + 0.5)* 255) # convert to regular image values
-            pred = Image.fromarray(pred)
-            pred.save('./outputs/pred_'+str(iteration_number)+'.bmp')
-            
-            bvae.ae.save('./output_models/'+str(iteration_number)+'_autoencoder.h5')
-            bvae.decoder.save('./output_models/'+str(iteration_number)+'_decoder.h5')
-            bvae.encoder.save('./output_models/'+str(iteration_number)+'_encoder.h5')
+
         
