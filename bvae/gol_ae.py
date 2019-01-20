ecf15be7aa944026ad385f791e355e657c3cc5d3
Mochi Liu <youxi3013@126.com>
2019-01-19 22:53:20 -0500

    production ready

--- bvae/gol_ae.py
+++ bvae/gol_ae.py
@@ -34,7 +34,7 @@ if __name__ == "__main__":
    
     manager = GameManager()
     batchSize = manager.sample_size
-    ntrain=1#number_of_training_samples//batchSize 
+    ntrain=50#number_of_training_samples//batchSize 
     nval=1#number_of_validation_samples//batchSize
     # This is how you build the autoencoder
     encoder = Encoder(inputShape, batchSize, latentSize, 'vae', beta=69, capacity=15, randomSample=True)
@@ -45,8 +45,8 @@ if __name__ == "__main__":
     iteration_number = 0
 
     while iteration_number < 100:
-        #bvae.ae.fit_generator(manager.generate_images(), steps_per_epoch=ntrain, workers=6, use_multiprocessing=True, validation_data=next(manager.generate_images()), validation_steps=nval, epochs=1,verbose=1)
-        bvae.ae.fit_generator(manager.generate_images(), steps_per_epoch=ntrain, workers=1, validation_data=next(manager.generate_images()), validation_steps=nval, epochs=1,verbose=1)
+        bvae.ae.fit_generator(manager.generate_images(), steps_per_epoch=ntrain, max_queuemax_queue_size=20, workers=6, use_multiprocessing=True, validation_data=next(manager.generate_images()), validation_steps=nval, epochs=1,verbose=1)
+        #bvae.ae.fit_generator(manager.generate_images(), steps_per_epoch=ntrain, workers=1, validation_data=next(manager.generate_images()), validation_steps=nval, epochs=1,verbose=1)
 
         sample_index = np.random.randint(batchSize)
         img = manager.get_images(batchSize)
@@ -56,14 +56,14 @@ if __name__ == "__main__":
         train = img[sample_index] #get a sample image
         train = np.uint8(train* 255) # convert to regular image values
         train = Image.fromarray(train)
-        #train.save('./outputs/train_'+str(iteration_number)+'.bmp')
-        train.save('.\\outputs\\train_'+str(iteration_number)+'.bmp')
+        train.save('./outputs/train_'+str(iteration_number)+'.bmp')
+        #train.save('.\\outputs\\train_'+str(iteration_number)+'.bmp')
         
         pred = bvae.ae.predict(img, batch_size=batchSize)[sample_index] # get the reconstructed image
         pred = np.uint8(pred * 255) # convert to regular image values
         pred = Image.fromarray(pred)
-        #pred.save('./outputs/pred_'+str(iteration_number)+'.bmp')
-        pred.save('.\\outputs\\pred_'+str(iteration_number)+'.bmp')
+        pred.save('./outputs/pred_'+str(iteration_number)+'.bmp')
+        #pred.save('.\\outputs\\pred_'+str(iteration_number)+'.bmp')
 
         if iteration_number % 10 == 0:
             bvae.ae.save('./output_models/'+str(iteration_number)+'_autoencoder.h5')
