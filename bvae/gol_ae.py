acceaac5057a9fc110adbc4fe570fe8fe7f9954c
Mochi Liu <youxi3013@126.com>
2020-05-19 23:44:27 -0400

    did not work back to convencoder used 48 latentsize which should be max

--- bvae/gol_ae.py
+++ bvae/gol_ae.py
@@ -76,8 +76,8 @@ if __name__ == "__main__":
         time.sleep(15) # wait for it to boot up
     inputShape = (32, 32, 3)
     intermediateSize = 64 #too many, 768 max
-    latentSize = 64
-    fast_multiplier = 9
+    latentSize = 48#64
+    fast_multiplier = 8
     msg = msg.replace(' ', '_').lower()
     msg = msg.splitlines()[0]
 
@@ -94,10 +94,10 @@ if __name__ == "__main__":
     manager = GameManager(batchSize, fast_multiplier)
     
     #conv autoencoder
-    encoder = OptimalEncoder(inputShape, batchSize, latentSize, intermediateSize, 'vae', beta=69, capacity=15, randomSample=True)
-    decoder = OptimalDecoder(inputShape, batchSize, latentSize, intermediateSize)
-    #encoder = ConvEncoder(inputShape, batchSize, latentSize, 'vae', beta=69, capacity=15, randomSample=True)
-    #decoder = ConvDecoder(inputShape, batchSize, latentSize)
+    #encoder = OptimalEncoder(inputShape, batchSize, latentSize, intermediateSize, 'vae', beta=69, capacity=15, randomSample=True)
+    #decoder = OptimalDecoder(inputShape, batchSize, latentSize, intermediateSize)
+    encoder = ConvEncoder(inputShape, batchSize, latentSize, 'vae', beta=69, capacity=15, randomSample=True)
+    decoder = ConvDecoder(inputShape, batchSize, latentSize)
     bvae = AutoEncoder(encoder, decoder)
 
     bvae.ae.compile(optimizer='adam', loss='binary_crossentropy')
